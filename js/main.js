eaca456f0ed96460f81d31558ffafbfa5d58eb9c
Galeano Santiago <galeanosantiago10@gmail.com>
2025-05-21 19:24:30 -0400

    Numeros de contacto

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 // Animacion de astronauta
 const astronaut = document.querySelector(".img-pet img");
-const titlePage = document.querySelector(".title-page");
+const titlePage = document.querySelectorAll(".title-page");
 const logo = document.querySelector(".img-logo img");
 const containerFooter = document.querySelector(".text-footer");
 
 astronaut.addEventListener("animationend", (e) => {
   if (e.animationName === "dropDown") {
     astronaut.style.animation = "float 4s ease-in-out infinite";
-    titlePage.style.opacity=1;
+    titlePage.forEach(title=>{
+      title.style.opacity=1;
+
+    })
     logo.style.opacity=1;
     containerFooter.style.opacity=1;
   }
