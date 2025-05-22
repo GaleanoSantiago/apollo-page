a4230df38e508436760f7513ecd70c11887cc6af
Galeano Santiago <galeanosantiago10@gmail.com>
2025-05-22 11:07:55 -0300

    correccion animacion del boton y alto en celulares

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const astronaut = document.querySelector(".img-pet img");
 const titlePage = document.querySelectorAll(".title-page");
 const logo = document.querySelector(".img-logo img");
+const btnMain = document.querySelector(".n_contacto_div .btn-main")
 const containerFooter = document.querySelector(".text-footer");
 
 astronaut.addEventListener("animationend", (e) => {
@@ -11,6 +12,7 @@ astronaut.addEventListener("animationend", (e) => {
       title.style.opacity=1;
 
     })
+    btnMain.style.opacity=1;
     logo.style.opacity=1;
     containerFooter.style.opacity=1;
   }
